c56bd3e687698ef8fb31cf0901877c375e09a302
TonyThuyTu <quyngaisensua@gmail.com>
2025-03-03 17:34:28 +0700

    done

--- test/bai1.js
+++ test/bai1.js
@@ -5,18 +5,15 @@
 // Cần thay thế 0 ở vị trí index = 7 ta có 
 // { 0, 0, 1, 0, 1, 1, 1, 1, 1, 1 } 
 // => có chuổi 6 phần tử 1 liên tiếp.
-
-function replaceZeroForMaxOnes(arr: number[]): number[] {
-    let maxLen = 0;
-    let bestIndex = -1;
-    let left = 0, zeroCount = 0;
-    
+function replaceZeroForMaxOnes(arr) {
+    var maxLen = 0;
+    var bestIndex = -1;
+    var left = 0, zeroCount = 0;
     // Tìm vị trí tốt nhất để thay đổi 0 thành 1
-    for (let right = 0; right < arr.length; right++) {
+    for (var right = 0; right < arr.length; right++) {
         if (arr[right] === 0) {
             zeroCount++;
         }
-
         // Nếu có nhiều hơn 1 số 0 trong cửa sổ, ta dịch left sang phải
         while (zeroCount > 1) {
             if (arr[left] === 0) {
@@ -24,24 +21,19 @@ function replaceZeroForMaxOnes(arr: number[]): number[] {
             }
             left++;
         }
-
         // Kiểm tra xem đây có phải dãy 1 dài nhất không
-        let currentLen = right - left + 1;
+        var currentLen = right - left + 1;
         if (currentLen > maxLen) {
             maxLen = currentLen;
             bestIndex = left; // Vị trí 0 tốt nhất nằm trong khoảng [left, right]
         }
     }
-
     // Nếu tìm thấy vị trí cần thay, thay thế nó trong mảng
     if (bestIndex !== -1) {
         arr[bestIndex] = 1;
     }
-
     return arr;
 }
-
 // Test case
-const n: number[] = [0, 0, 1, 0, 1, 1, 1, 0, 1, 1];
+var n = [0, 0, 1, 0, 1, 1, 1, 0, 1, 1];
 console.log(replaceZeroForMaxOnes(n));
-
--- test/bai2.js
+++ test/bai2.js
@@ -5,37 +5,30 @@
 // sub-array có tổng = 8 bao gồm:
 //  { -5, 5, 3, 5 }{ 3, 5 }{ 5, 3 } 
 // => sub-array dài nhất là: { -5, 5, 3, 5 } có length = 4
-
-function longestSubArrayWithSum(nums: number[], target: number): number[] {
-    let sumIndex: { [key: number]: number } = {}; // Dùng object thay Map
-    let prefixSum = 0;
-    let maxLen = 0;
-    let startIndex = -1, endIndex = -1;
-
+function longestSubArrayWithSum(nums, target) {
+    var sumIndex = {}; // Dùng object thay Map
+    var prefixSum = 0;
+    var maxLen = 0;
+    var startIndex = -1, endIndex = -1;
     sumIndex[0] = -1; // Xử lý trường hợp tổng bằng target ngay từ đầu
-
-    for (let i = 0; i < nums.length; i++) {
+    for (var i = 0; i < nums.length; i++) {
         prefixSum += nums[i];
-
         if ((prefixSum - target) in sumIndex) {
-            let prevIndex = sumIndex[prefixSum - target];
-            let length = i - prevIndex;
-            if (length > maxLen) {
-                maxLen = length;
+            var prevIndex = sumIndex[prefixSum - target];
+            var length_1 = i - prevIndex;
+            if (length_1 > maxLen) {
+                maxLen = length_1;
                 startIndex = prevIndex + 1;
                 endIndex = i;
             }
         }
-
         if (!(prefixSum in sumIndex)) {
             sumIndex[prefixSum] = i;
         }
     }
-
     return startIndex !== -1 ? nums.slice(startIndex, endIndex + 1) : [];
 }
-
 // Test case
-const num: number[] = [5, 6, -5, 5, 3, 5, 3, -2, 0];
-const target = 8;
+var num = [5, 6, -5, 5, 3, 5, 3, -2, 0];
+var target = 8;
 console.log(longestSubArrayWithSum(num, target));
